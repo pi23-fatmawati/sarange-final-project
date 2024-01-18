560b47f4d538bc1a0cba56afdfb4f51e0df64e2e
llatifah99 <litalatifah6@gmail.com>
2024-01-18 22:50:10 +0700

    styling di register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
         alignItems: "center",
         justifyContent: "center",
         height: "100vh",
-        marginTop: "25px",
+        marginTop: "50px",
       }}
     >
       <img
@@ -35,8 +35,17 @@ const Register = () => {
           marginRight: "50px",
         }}
       />
-      <div className="container mx-auto mt-10 border p-4 rounded-md">
-        <div style={{ display: "flex", alignItems: "center" }}>
+      <div
+        className="container mx-auto mt-10 border p-4 rounded-md"
+        style={{ flex: 1, margin: "0 40px", maxWidth: "450px" }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            margin: " 25px 30px 20px 30px",
+          }}
+        >
           <h1
             style={{
               fontSize: "1.5rem",
@@ -52,61 +61,74 @@ const Register = () => {
             style={{ height: "100%", marginLeft: "auto" }}
           />
         </div>
-        <form>
-          <div className="mb-4">
-            <input
-              type="text"
-              placeholder="Nama"
-              className="mt-1 p-2 w-full border rounded-md"
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              type="email"
-              placeholder="Email"
-              className="mt-1 p-2 w-full border rounded-md"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              type="password"
-              placeholder="Katasandi"
-              className="mt-1 p-2 w-full border rounded-md"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              type="password"
-              placeholder="Konfirmasi Sandi"
-              className="mt-1 p-2 w-full border rounded-md"
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </div>
-          <div className="mb-4 flex items-center">
-            <input
-              type="checkbox"
-              className="mr-2"
-              checked={agreement}
-              onChange={() => setAgreement(!agreement)}
-            />
-            <label className="text-sm font-medium text-gray-600">
-              Saya menyetujui syarat dan ketentuan yang berlaku
-            </label>
-          </div>
-          <button
-            type="button"
-            className="bg-blue-500 text-white px-4 py-2 rounded-md"
-            onClick={handleRegister}
-          >
-            Daftar
-          </button>
-        </form>
-        <p className="mt-4">
-          Sudah punya akun? <Link to="/login">MASUK</Link>
-        </p>
+        <div style={{ margin: " 25px 30px 20px 30px" }}>
+          <form>
+            <div className="mb-4">
+              <input
+                type="text"
+                placeholder="Nama"
+                className="mt-1 p-2 w-full border rounded-md"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="email"
+                placeholder="Email"
+                className="mt-1 p-2 w-full border rounded-md"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="password"
+                placeholder="Katasandi"
+                className="mt-1 p-2 w-full border rounded-md"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="password"
+                placeholder="Konfirmasi Sandi"
+                className="mt-1 p-2 w-full border rounded-md"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+            <div className="mb-4 flex items-center">
+              <input
+                type="checkbox"
+                className="mr-2"
+                checked={agreement}
+                onChange={() => setAgreement(!agreement)}
+              />
+              <label className="text-sm font-medium text-gray-600">
+                Saya menyetujui syarat dan ketentuan yang berlaku
+              </label>
+            </div>
+            <button
+              type="button"
+              className=" text-white w-full px-4 py-2 rounded-md"
+              style={{ backgroundColor: "#52C41A" }}
+              onClick={handleRegister}
+            >
+              Daftar
+            </button>
+          </form>
+          <p className="mt-4">
+            Sudah punya akun?{" "}
+            <Link
+              to="/login"
+              style={{
+                color: "#52C41A",
+                fontWeight: "bold",
+                textDecoration: "underline",
+              }}
+            >
+              MASUK
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
