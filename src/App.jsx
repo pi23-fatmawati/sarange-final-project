9782445ee5b2e4b8298f34e41d69f0812cf068bc
llatifah99 <litalatifah6@gmail.com>
2024-01-21 22:52:51 +0700

    update profile user

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import "./App.css";
+import React from "react";
 import UserProfile from "./components/UserProfile";
-function App() {
+
+const App = () => {
+  const userData = {
+    nama: "Fatmawati",
+    email: "fatmawati@sarange.com",
+    nomorHp: "123-456-7890",
+    alamat: "Jln Ahmad Yani No 99 Surabaya Jawa Timur",
+  };
+
   return (
-    <>
-      <h1>ini halaman profile user Sarange</h1>
-      <UserProfile />
-    </>
+    <div>
+      <UserProfile userData={userData} />
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,39 +1,70 @@
 import React from "react";
-import userImage from "../pic/profilepic.png";
+import userImage from "../pic/profilepic0.png";
 
-const UserProfile = () => {
+const UserProfile = ({ userData }) => {
   return (
-    <div className="max-w-md mx-auto bg-white rounded p-6 shadow-md">
-      <h2 className="text-2xl font-semibold mb-4">Informasi Pengguna</h2>
-      <div>
-        <div className="mb-2">
-          <label className="block text-gray-600">Nama</label>
-          <p className="text-gray-600">Nama Pengguna</p>
-        </div>
-        <div className="mb-2">
-          <label className="block text-gray-600">Email</label>
-          <p className="text-gray-600">user@example.com</p>
-        </div>
-        <div className="mb-2">
-          <label className="block text-gray-600">Nomor HP</label>
-          <p className="text-gray-600">123-456-7890</p>
-        </div>
-        <div className="mb-2">
-          <label className="block text-gray-600">Alamat</label>
-          <p className="text-gray-600">Jalan Contoh No. 123</p>
-        </div>
-        <label className="block text-gray-600">Foto</label>
-        <div className="mb-2">
-          <img
-            src={userImage}
-            alt="User Image"
-            className="w-16 h-16 rounded-full mr-4"
-          />
+    <div className="max-w-screen-s-m-xl mx-32 bg-white rounded border">
+      <div className="border-b px-6 py-3">
+        <h2 className="text-xl font-semibold">Informasi Pengguna</h2>
+      </div>
+      <div className="p-6">
+        <div>
+          <div className="flex mb-2">
+            <label className="block text-gray-600 w-1/4">Nama</label>
+            <input
+              type="text"
+              defaultValue={userData.nama}
+              className="text-gray-600 w-3/4 border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:border-blue-500"
+              readOnly
+            />
+          </div>
+          <div className="flex mb-2">
+            <label className="block text-gray-600 w-1/4">Email</label>
+            <input
+              type="email"
+              defaultValue={userData.email}
+              className="text-gray-600 w-3/4 border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:border-blue-500"
+              readOnly
+            />
+          </div>
+          <div className="flex mb-2">
+            <label className="block text-gray-600 w-1/4">Nomor HP</label>
+            <input
+              type="tel"
+              defaultValue={userData.nomorHp}
+              className="text-gray-600 w-3/4 border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:border-blue-500"
+              readOnly
+            />
+          </div>
+          <div className="flex mb-2">
+            <label className="block text-gray-600 w-1/4">Alamat</label>
+            <textarea
+              defaultValue={userData.alamat}
+              className="text-gray-600 w-3/4 border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:border-blue-500"
+              readOnly
+            />
+          </div>
+          <div className="flex mb-2">
+            <label className="block text-gray-600 w-1/4">Foto</label>
+            <div className="w-3/4">
+              <img
+                src={userImage}
+                alt="User Image"
+                className="w-30 h-30 rounded-full mr-4"
+              />
+              <label>svg, png, jpg or gif (max 800x400px)</label>
+            </div>
+          </div>
         </div>
       </div>
-      <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-        Edit Profil
-      </button>
+      <div
+        style={{ backgroundColor: "#FBFBFB" }}
+        className="px-4 py-4 rounded p-0 flex justify-end border-t"
+      >
+        <button className="bg-custom-green hover:bg-blue-600 text-white px-4 py-2 rounded">
+          Edit Profil
+        </button>
+      </div>
     </div>
   );
 };
