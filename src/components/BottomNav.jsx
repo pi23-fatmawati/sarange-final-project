470433cc4f5a97b13f0141df62029964eb9d8e17
llatifah99 <litalatifah6@gmail.com>
2024-01-22 11:30:52 +0700

    pisah file css

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,20 +1,37 @@
+import React from "react";
+import "./BottomNav.css";
+
 const BottomNav = () => {
   return (
-    <div className="fixed bottom-0 left-0 z-50 w-full h-28 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+    <div className={`bottom-nav`}>
       <div className="flex h-full items-center justify-between max-w-lg-xl mx-auto font-medium">
-        <label className="ml-8">Total Produk:</label>
-        <label className="">Total Koin:</label>
-        <button
-          className=" text-white px-4 py-2 rounded mr-8"
-          style={{
-            backgroundColor: "#52C41A",
-          }}
-        >
-          Atur Jadwal
-        </button>
+        <label className="bottom-nav-label">Total Produk:</label>
+        <label className="bottom-nav-label">Total Koin:</label>
+        <button className="bottom-nav-button">Atur Jadwal</button>
       </div>
     </div>
   );
 };
 
 export default BottomNav;
+
+// const BottomNav = () => {
+//   return (
+//     <div className="fixed bottom-0 left-0 z-50 w-full h-28 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+//       <div className="flex h-full items-center justify-between max-w-lg-xl mx-auto font-medium">
+//         <label className="ml-8">Total Produk:</label>
+//         <label className="">Total Koin:</label>
+//         <button
+//           className=" text-white px-4 py-2 rounded mr-8"
+//           style={{
+//             backgroundColor: "#52C41A",
+//           }}
+//         >
+//           Atur Jadwal
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default BottomNav;
