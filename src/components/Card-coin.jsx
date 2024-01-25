aa9981128a605ddba8ad5d4ad51cd17d3ae911d9
Marliana Azizah <marlianaazizah36@gmail.com>
2024-01-25 12:21:43 +0800

    edit card coin

--- src/components/Card-coin.jsx
+++ src/components/Card-coin.jsx
@@ -5,7 +5,7 @@ import './component.css'
 export default function CardCoin(props){
     return (
         <div className="w-full flex justify-center">
-            <Link to={'/history-coin'} className="card-coin flex flex-col items-center justify-center gap-2 w-fit mt-24 w-72 h-28">
+            <Link to={'/history-coin'} className="card-coin flex flex-col items-center justify-center gap-2 w-fit mt-24 w-72 h-28 p-4">
                 <p className='text-center font-medium'>Koin Anda</p>
                 <div className="card-coin-content flex gap-2 items-center">
                     <img className='w-auto max-h-12' src={Coin} alt="coin image" />
