f5f316506e21da148f4aac34982440a46788fa1e
llatifah99 <litalatifah6@gmail.com>
2024-02-11 06:40:16 +0700

    transaction process and confirm

--- src/components/HistoryProcess.jsx
+++ src/components/HistoryProcess.jsx
@@ -1,53 +1,113 @@
 import "./component.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Pagination from "./Pagination";
 import ButtonGreen from "./Button-green";
 import ButtonYellow from "./ButtonYellow";
 import ButtonOutline from "./Button-outline";
 import { Link } from "react-router-dom";
 
-const HistoryProcess = () => {
-  const itemsPerPage = 10; //banyak data yang tampil tiap page
+const History = () => {
+  const itemsPerPage = 4; // Banyak data yang tampil tiap halaman
   const [currentPage, setCurrentPage] = useState(1);
+  const [transactionData, setTransactionData] = useState([]);
 
-  const transactionData = [
-    {
-      id: 1,
-      tanggalTransaksi: "2024-01-23",
-      waktuPenjemputan: "10:00",
-      produk: "Botol Plastik, Botol Kaca",
-      koin: 250,
-      status: "Diproses",
-    },
-    {
-      id: 2,
-      tanggalTransaksi: "2024-01-22",
-      waktuPenjemputan: "08:00",
-      produk: "Botol kaca",
-      koin: 30,
-      status: "Diproses",
-    },
-    {
-      id: 3,
-      tanggalTransaksi: "2024-01-21",
-      waktuPenjemputan: "12:00",
-      produk: "Karung, plastik bag",
-      koin: 200,
-      status: "Konfirmasi",
-    },
-  ];
+  // Mengambil data transaksi dari endpoint
+  useEffect(() => {
+    const fetchProcessData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await fetch(
+          "https://final-sarange-eff62c954ab5.herokuapp.com/transaction/process",
+          {
+            headers: {
+              authorization: `${token}`,
+            },
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+
+        // Memeriksa apakah ada data transaksi yang diterima dari API
+        if (Array.isArray(data.transactions) && data.transactions.length > 0) {
+          setTransactionData((prevData) => [...prevData, ...data.transactions]);
+        } else {
+          console.warn("Tidak ada data transaksi proses yang tersedia.");
+        }
+      } catch (error) {
+        console.error("Error fetching process transaction data:", error);
+      }
+    };
+
+    const fetchConfirmData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await fetch(
+          "https://final-sarange-eff62c954ab5.herokuapp.com/transaction/confirm",
+          {
+            headers: {
+              authorization: `${token}`,
+            },
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+
+        // Memeriksa apakah ada data transaksi yang diterima dari API
+        if (Array.isArray(data.transactions) && data.transactions.length > 0) {
+          setTransactionData((prevData) => [...prevData, ...data.transactions]);
+        } else {
+          console.warn("Tidak ada data transaksi konfirmasi yang tersedia.");
+        }
+      } catch (error) {
+        console.error("Error fetching confirm transaction data:", error);
+      }
+    };
+
+    fetchProcessData();
+    fetchConfirmData();
+  }, []);
 
   // Calculate the total number of pages
   const totalPages = Math.ceil(transactionData.length / itemsPerPage);
 
+  // Sort transactions by status: Konfirmasi -> Diproses
+  const sortedTransactions = transactionData.sort((a, b) => {
+    if (a.status === "Konfirmasi" && b.status === "Diproses") return -1;
+    if (a.status === "Diproses" && b.status === "Konfirmasi") return 1;
+    return 0;
+  });
+
   // Calculate the starting and ending index of the transactions for the current page
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = Math.min(startIndex + itemsPerPage, transactionData.length);
+  const endIndex = Math.min(
+    startIndex + itemsPerPage,
+    sortedTransactions.length
+  );
 
-  const currentTransactions = transactionData.slice(startIndex, endIndex);
+  const currentTransactions = sortedTransactions.slice(startIndex, endIndex);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
+  };
+
+  const confirmTransactionById = async (transactionId) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await fetch(
+        `https://final-sarange-eff62c954ab5.herokuapp.com/transaction/${transactionId}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `${token}`,
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data); // Handle response data accordingly
+    } catch (error) {
+      console.error("Error confirming transaction:", error);
+    }
   };
 
   return (
@@ -69,27 +129,33 @@ const HistoryProcess = () => {
                 key={index}
                 className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
               >
-                <td className="px-6 py-4">{transaction.tanggalTransaksi}</td>
-                <td className="px-6 py-4">{transaction.produk}</td>
-                <td className="px-6 py-4">{transaction.koin}</td>
                 <td className="px-6 py-4">
-                  {transaction.status === "Konfirmasi" ? (
-                    <ButtonGreen text="Konfirmasi" padding="px-4" />
-                  ) : (
+                  {new Date(transaction.pickup_date).toLocaleDateString()}
+                </td>
+                <td className="px-6 py-4">
+                  {transaction.Cart.Product.product_name}
+                </td>
+                <td className="px-6 py-4">{transaction.Cart.total_coin}</td>
+                <td className="px-6 py-4">
+                  {transaction.status === "Diproses" ? (
                     <ButtonYellow disabled text="Diproses" />
+                  ) : (
+                    <ButtonGreen
+                      text="Dikonfirmasi"
+                      padding="px-4"
+                      onClick={() =>
+                        confirmTransactionById(transaction.id_transaction)
+                      }
+                    />
                   )}
                 </td>
                 <td className="px-6 py-4">
-                  <Link
-                    to={`/sell/transactions/${transaction.id}?tanggalTransaksi=${transaction.tanggalTransaksi}&waktuPenjemputan=${transaction.waktuPenjemputan}&produk=${transaction.produk}&koin=${transaction.koin}&status=${transaction.status}`}
-                  >
-                    <ButtonOutline
-                      text="Cek Detail"
-                      width="w-max"
-                      padding="px-4"
-                      // onClick={() => console.log(transaction)}
-                    />
-                  </Link>
+                  <ButtonOutline
+                    text="Cek Detail"
+                    width="w-max"
+                    padding="px-4"
+                    disabled
+                  />
                 </td>
               </tr>
             ))}
@@ -108,4 +174,4 @@ const HistoryProcess = () => {
   );
 };
 
-export default HistoryProcess;
+export default History;
