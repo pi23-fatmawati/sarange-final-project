863921a2f8b5132e20f164cf69a5aa0f0532aa80
Sri Fitri Yani Nurhasanah <044592357@ecampus.ut.ac.id>
2024-02-13 09:06:56 +0700

    change paths for login components

--- public/pages/Login.jsx
+++ public/pages/Login.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "../components/component.css";
-import "../App.css";
+import "../../src/components/component.css";
+import "../../src/App.css";
 import Logo from "../pic/Logo.png";
-import Shopping from "../pic/Shopping.png";
+import Shopping from "../../src/pic/Shopping.png";
 import NavbarRegisterLogin from "../../src/components/NavbarRegisterLogin";
 import { useDispatch, useSelector } from "react-redux";
 import {
